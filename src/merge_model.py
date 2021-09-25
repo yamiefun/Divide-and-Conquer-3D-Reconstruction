de0a257bfcdf83b2ec5a3fd1e601cf23597ce93d
YunmingChan <yunming.chan@gmail.com>
2021-09-25 14:23:55 +0800

    find transformation matrix with minimum error

--- src/merge_model.py
+++ src/merge_model.py
@@ -57,10 +57,11 @@ def ransac_find_transform(matched_3d_set, blk1, blk2, thresh):
         tmp_err = mm.calculate_err(tmp_mtx, matched_set, blk1, blk2)
         # print(tmp_err)
         if tmp_err < err:
+            err = tmp_err
             mtx = tmp_mtx
             # R = ret_R
             # t = ret_t
-    print(f"Best mapping outlier ratio: {tmp_err/len(matched_set)}")
+    print(f"Best mapping outlier ratio: {err/len(matched_set)}")
     # return R, t
     return mtx
 
